79a3f84e92c96175eaab8d82fc7de63e10ba00b3
chottu <chottu100@yahoo.com>
2020-06-04 15:14:12 -0300

    first commit

---
+++ src/test/java/qucikclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,13 @@
+package qucikclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void appiumtest()
+	{
+		System.out.println("this is a test for Appium");
+	}
+
+}
---
+++ src/test/java/qucikclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,14 @@
+package qucikclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	
+	public void browserAutomation()
+	{
+		System.out.println("this is a test with Maven");
+	}
+
+}
